4bbc20b0ca3c5c8c1466971d2c8da9bff98595f3
francesco <fg.minisini@gmail.com>
2023-12-05 13:39:36 +0100

    lab10

---
+++ librerie/random/random.h
@@ -0,0 +1,14 @@
+#include<cstdlib>
+#include<iostream>
+using namespace std ; 
+#include<cmath>
+#include<strings.h>
+#include<fstream>
+#include <ctime>
+#define PI 3.14159
+
+double rand_uniforme(double minimo, double massimo);
+
+double rand_gaussiano (double media,  double std);
+
+void imposta_seed ();
---
+++ week9/es2.C
@@ -0,0 +1,65 @@
+
+#include "/home/francesco/programmazione/gituni/librerie/random/random.h"
+
+struct puntoR2 {
+    double x; 
+    double y; 
+};
+bool is_inside_circle (puntoR2 p, double r );
+double distance ( puntoR2 p );
+int count_whithin ( int n , double r);
+double rilev(int n, double r);
+
+int main ()  {
+    imposta_seed ();
+    double res;  
+    double r = 1;
+    double n = 100000000; 
+    res =  rilev (n , r);
+    cout << endl << res << endl ;
+    return 0; 
+}
+
+
+double distance ( puntoR2 p1, puntoR2 p2  ) { 
+    double d; 
+    d = sqrtf64x( powf64((p1.x-p2.x) , 2) + powf64( (p1.y - p2.y) , 2));
+    return d;
+}
+
+
+bool is_inside_circle (puntoR2 p, double r ){
+    bool B; 
+    puntoR2 O;
+    O.x = 0, O.y =0 ; 
+    double d  = distance ( p , O );
+
+    if (r < d) {
+        B = false;
+    } else {
+        B = true;
+    }
+
+    return B;
+}
+
+int count_whithin ( int n , double r) {
+    puntoR2 test; 
+    int res = 0; 
+    for (int i = 0; i < n; i++)
+    {
+        test.x = rand_uniforme (0 , r);
+        test.y = rand_uniforme ( 0 , r); 
+        if (is_inside_circle (test , r)) {
+            res++ ; 
+        }
+    }
+    return res;
+}
+
+double rilev(int n, double r) {
+    double res; 
+    res = (double)count_whithin (n , r)*4 / n; 
+    return res; 
+}
+
