1ecc6169650af336e37b318736de02329d3106e4
francesco <fg.minisini@gmail.com>
2024-09-25 17:36:46 +0200

    Lezioone1

---
+++ 2023/Lezione1.C
@@ -0,0 +1,34 @@
+#include<iostream>
+using namespace std ; 
+#include<cmath>
+#include<cstring>
+#include<fstream>
+#include"librerie/funzioni/funzioni.h"
+
+int main(){
+if (argc < 3) {
+    cout << "\nToo few parameters executing file: " << kargs[0];
+    return 1; // Exit if there are not enough arguments
+}
+
+    string filePath = kargs[1];
+    ifstream data (filePath);
+if (!data) {
+    cout << "\ncannot open file: " << filePath;
+    return 1; // Exit if the file cannot be opened
+}
+
+    double media = 0;
+    int count = 0;
+    int upperBound = stoi(kargs[2]);
+    double temp = 0; 
+    while ( !data.eof() && count < upperBound){
+        data>> temp;
+        media += temp/(double)upperBound;
+        count++;
+    }
+    cout<< "Result: "<<  media <<"\n";
+
+    
+    return 0;
+}
