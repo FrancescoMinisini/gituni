147a92dd2f98440d8712ea0436234f867052c5e0
francesco <fg.minisini@gmail.com>
2024-11-02 18:09:56 +0100

    Esercizio 3

---
+++ 2023/Lezione6/Esercizio2/build/CMakeFiles/Esercizio2.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Esercizio2.dir/main.cpp.o"
+  "CMakeFiles/Esercizio2.dir/main.cpp.o.d"
+  "Esercizio2"
+  "Esercizio2.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Esercizio2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ 2023/Lezione6/Esercizio3/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <vector>
+#include <cmath>
+#include <fstream>
+#include <iomanip>
+#include "Models/FunzioneBase.h"
+#include <cstdlib>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+  FunzioneBase *par1 = new Parabola(1, 0, -1);
+  par1->Plot("Plotting");
+  return 0;
+}
